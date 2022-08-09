18e9f5feb4538549e1885683034f39d16e0ed468
Omkar Shinde <Omkar.Shinde@ril.com>
2022-08-09 20:23:12 +0530

    In GameFrame.java file line 5 uncommented

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
     GameFrame(){
-        //this.add(new GamePanel()); // making reference to the GamePanel class
+        this.add(new GamePanel()); // making reference to the GamePanel class
         this.setTitle("Snake Mania"); // Setting game window title
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Stopping the program when user clicks on close button
         this.setResizable(false); // Making game window not resizable
